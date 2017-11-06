ba447fd87a3b901a38080388ffa2777d5f99f23a
kerytamisha <keryta.mi@gmail.com>
2017-11-06 09:56:29 +0200

    My homework about a block

---
+++ Block_homework/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Block_homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double  holeHeight = Convert.ToDouble(Console.ReadLine()),
+                    holeWidth = Convert.ToDouble(Console.ReadLine()),
+                    blockX = Convert.ToDouble(Console.ReadLine()),
+                    blockY = Convert.ToDouble(Console.ReadLine()),
+                    blockZ = Convert.ToDouble(Console.ReadLine());
+            if (holeWidth <=0 || holeHeight<=0)
+            {
+                Console.WriteLine("It is not a hole.Error");
+                return;
+            }
+            if (blockX<=0 || blockY<=0 || blockZ <=0)
+            {
+                Console.WriteLine("It is not  a block.Error");
+                return;
+            }
+            if ((holeHeight >= blockX && holeWidth >= blockZ)||
+                (holeHeight >= blockX && holeWidth >= blockY)||
+                (holeHeight >= blockY && holeWidth >= blockZ)||
+                (holeHeight >= blockZ && holeWidth >= blockX)||
+                (holeHeight >= blockY && holeWidth >= blockX)||
+                (holeHeight >= blockZ && holeWidth >= blockY))
+            {
+                Console.WriteLine("YES"); 
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
